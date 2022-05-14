488a9e79ed97b6e38c6e5a446b5a320eead71098
iamzhanghaohui <939026891@qq.com>
2022-05-14 17:40:03 +0800

    fix:fix constraint

--- image-matching/image-matching.go
+++ image-matching/image-matching.go
@@ -1,6 +1,12 @@
-package image_matching
+package main
 
-import "github.com/consensys/gnark/frontend"
+import (
+	"fmt"
+	"github.com/consensys/gnark-crypto/ecc"
+	"github.com/consensys/gnark/backend/groth16"
+	"github.com/consensys/gnark/frontend"
+	"github.com/consensys/gnark/frontend/cs/r1cs"
+)
 
 const (
 	ImageW  = 5
@@ -20,43 +26,74 @@ type Output struct {
 
 //todo what do u want to prove? which variable is public?
 type Circuit struct {
-	image  []frontend.Variable
+	image  []frontend.Variable `gnark:",public"`
 	kernel []frontend.Variable
-	output Output
+	output Output `gnark:",public"`
 }
 
 func (circuit *Circuit) diff(x, y int, api frontend.API) frontend.Variable {
-	total_diff := frontend.Variable(0)
+	totalDiff := frontend.Variable(0)
 	for kpx := 0; kpx < KernelH; kpx++ {
 		for kpy := 0; kpy < KernelW; kpy++ {
-			image_pixel := circuit.image[(kpx+x)*ImageH+(kpx+y)*ImageW]
-			kernel_pixel := circuit.kernel[kpy*KernelH+kpx]
-			local_diff := api.Div(image_pixel, kernel_pixel)
+
+			localDiff := api.Div(circuit.image[ImageW], circuit.kernel[KernelH])
 			//abs
-			local_diff = api.Select(api.Cmp(local_diff, 0), api.Neg(local_diff), local_diff)
-			total_diff = api.Add(total_diff, local_diff)
+			localDiff = api.Select(api.Cmp(localDiff, 0), api.Neg(localDiff), localDiff)
+			totalDiff = api.Add(totalDiff, localDiff)
 		}
 	}
-	return total_diff
+	return totalDiff
 }
 
 func (circuit *Circuit) Define(api frontend.API) error {
-	circuit.output.Point.y = 0
-	circuit.output.Point.x = 0
-	circuit.output.min_delta = circuit.diff(0, 0, api)
 
+	temp := circuit.diff(0, 0, api)
+	var tempy, tempx frontend.Variable
 	for y := 0; y < ImageH-KernelH+1; y++ {
 		for x := 0; x < ImageW-KernelW+1; x++ {
 			delta := circuit.diff(x, y, api)
-			circuit.output.Point.y = api.Select(api.Cmp(delta, circuit.output.min_delta), circuit.output.Point.y, frontend.Variable(y))
-			circuit.output.Point.x = api.Select(api.Cmp(delta, circuit.output.min_delta), circuit.output.Point.x, frontend.Variable(x))
-			circuit.output.min_delta = api.Select(api.Cmp(delta, circuit.output.min_delta), circuit.output.min_delta, delta)
+			tempy = api.Select(api.Cmp(delta, temp), circuit.output.Point.y, frontend.Variable(y))
+			tempx = api.Select(api.Cmp(delta, temp), circuit.output.Point.x, frontend.Variable(x))
+			temp = api.Select(api.Cmp(delta, temp), temp, delta)
 		}
 	}
-
+	api.AssertIsEqual(circuit.output.min_delta, temp)
+	api.AssertIsEqual(circuit.output.Point.x, tempx)
+	api.AssertIsEqual(circuit.output.Point.y, tempy)
 	return nil
 }
 
 func main() {
 
+	output := Output{
+		Point:     Point{x: frontend.Variable(1), y: frontend.Variable(2)},
+		min_delta: frontend.Variable(0),
+	}
+
+	witness := Circuit{
+		image:  make([]frontend.Variable, ImageW*ImageH),
+		kernel: make([]frontend.Variable, KernelW*KernelH),
+		output: output,
+	}
+
+	for i := 0; i < ImageW*ImageH; i++ {
+		witness.image[i] = 1
+	}
+	for i := 0; i < KernelW*KernelH; i++ {
+		witness.kernel[i] = 1
+	}
+
+	validWitness, err := frontend.NewWitness(&witness, ecc.BN254)
+
+	validPublicWitness, err := frontend.NewWitness(&witness, ecc.BN254, frontend.PublicOnly())
+	ccs, err := frontend.Compile(ecc.BN254, r1cs.NewBuilder, &witness)
+	pk, vk, err := groth16.Setup(ccs)
+
+	proof2, err := groth16.Prove(ccs, pk, validWitness)
+	err = groth16.Verify(proof2, vk, validPublicWitness)
+	if err != nil {
+		fmt.Print("not ok")
+	} else {
+		fmt.Print("ok")
+	}
 }
